cd04e95974e8f92bbe7ea07d18971d4c584381ae
Joseph Wang <joseph_wang@ettoday.net>
2020-02-08 02:01:21 +0800

    test ci

--- app/src/main/java/dev/weiqi/androidtravisci/MainActivity.kt
+++ app/src/main/java/dev/weiqi/androidtravisci/MainActivity.kt
@@ -2,11 +2,13 @@ package dev.weiqi.androidtravisci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("Joseph", "ci")
     }
 }
